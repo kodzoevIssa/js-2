a2ae5ec6d02183ead58770713b0e2777a79b74c5
Исса <108079241+kodzoevIssa@users.noreply.github.com>
2023-03-19 17:12:09 +0300

    js-2
    js-2

---
+++ Документы/4.1.js
@@ -0,0 +1,13 @@
+let table = document.getElementById("age-table");
+
+table.getElementsByTagName("label");
+
+table.rows[0].cells[0];
+
+table.getElementsByTagName("td")[0];
+
+let form = document.getElementsByName("search")[0];
+
+form.getElementsByTagName("input")[0];
+
+form.querySelector("input");
